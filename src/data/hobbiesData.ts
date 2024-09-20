0c650fdbe90834a16b560d94f256d731e8e719d4
Luan Vitor Coelho de Araujo <luan.araujo@genesisgenomics.com.br>
2024-09-20 10:09:25 -0300

    Update hobbiesData.ts

--- src/data/hobbiesData.ts
+++ src/data/hobbiesData.ts
@@ -1,35 +1,34 @@
 export const hobbiesData = [
-    { 
-      id: 1, 
-      title: 'Fórmula 1', 
-      description: 'Comecei a acompanhar a Fórmula 1 há dois anos, impulsionado por uma série da Netflix que despertou meu interesse por esse esporte de alta adrenalina. Desde então, minha paixão só cresceu! Torço fervorosamente pela McLaren e pelo talento do jovem piloto Lando Norris. Este novo amor pelo automobilismo também me levou a explorar o automobilismo virtual.', 
-      bgImage: '/images/formula1.jpg',
-      images: ['/images/interlakes2.jpg', '/images/interlakes.jpg', '/images/lego.jpg'],
-      cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🏎️</text></svg>'), auto"
-    },
-    { 
-      id: 2, 
-      title: 'Skate', 
-      description: 'Desde cedo, o skate sempre foi uma forma de liberdade e expressão para mim. Andar de skate me permite explorar a cidade de uma maneira única, encontrando novos desafios e superando meus próprios limites.', 
-      bgImage: '/images/skate.jpg',
-      images: ['/images/skate_1.jpg', '/images/skate_2.jpg', '/images/skate_3.jpg'],
-      cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🛹</text></svg>'), auto"
-    },
-    { 
-      id: 6, 
-      title: 'Música', 
-      description: 'A música sempre foi uma parte essencial da minha vida. Seja tocando um instrumento, cantando, ou simplesmente ouvindo minhas playlists favoritas, a música me ajuda a me conectar comigo mesmo e com o mundo ao meu redor.', 
-      bgImage: '/images/music.jpg',
-      images: ['/images/music_1.jpg', '/images/music_2.jpg', '/images/music_3.jpg'],
-      cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🎵</text></svg>'), auto"
-    },
-    { 
-      id: 7, 
-      title: 'Videogames', 
-      description: 'Videogames são mais do que apenas uma diversão para mim; são uma forma de explorar novos mundos, enfrentar desafios e socializar com amigos.', 
-      bgImage: '/images/videogames.jpg',
-      images: ['/images/videogames_1.jpg', '/images/videogames_2.jpg', '/images/videogames_3.jpg'],
-      cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🎮</text></svg>'), auto"
-    },
-  ];
-  
+  { 
+    id: 1, 
+    title: 'Fórmula 1', 
+    description: 'Comecei a acompanhar a Fórmula 1 há dois anos, impulsionado por uma série da Netflix que despertou meu interesse por esse esporte de alta adrenalina. Desde então, minha paixão só cresceu! Torço fervorosamente pela McLaren e pelo talento do jovem piloto Lando Norris. Este novo amor pelo automobilismo também me levou a explorar o automobilismo virtual.', 
+    bgImage: '/images/formula1.jpg',
+    images: ['/images/interlakes2.jpg', '/images/interlakes.jpg', '/images/lego.jpg'],
+    cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🏎️</text></svg>'), auto"
+  },
+  { 
+    id: 2, 
+    title: 'Skate', 
+    description: 'Comecei a andar de skate aos 9 anos de idade por influência do meu pai. Apesar de ter parado de praticar há alguns anos, aprendi muito com o skate e ainda gosto bastante da cultura envolvida.', 
+    bgImage: '/images/skate.jpg',
+    images: ['/images/skate_1.jpg', '/images/skate_2.jpg', '/images/skate_3.jpg'],
+    cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🛹</text></svg>'), auto"
+  },
+  { 
+    id: 6, 
+    title: 'Música', 
+    description: 'A música veio muito do skate. O estilo, as bandas e os sons que eu escuto hoje têm uma forte conexão com essa cultura que me influenciou bastante desde que comecei a andar de skate.', 
+    bgImage: '/images/music.jpg',
+    images: ['/images/music_1.jpg', '/images/music_2.jpg', '/images/music_3.jpg'],
+    cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🎵</text></svg>'), auto"
+  },
+  { 
+    id: 7, 
+    title: 'Videogames', 
+    description: 'Os videogames sempre estiveram presentes na minha vida desde muito novo e nunca saíram. É onde gosto de gastar meu tempo livre, seja para me divertir ou para relaxar.', 
+    bgImage: '/images/videogames.jpg',
+    images: ['/images/videogames_1.jpg', '/images/videogames_2.jpg', '/images/videogames_3.jpg'],
+    cursor: "url('data:image/svg+xml,<svg xmlns=\"http://www.w3.org/2000/svg\" width=\"32\" height=\"32\"><text y=\"20\" font-size=\"20\">🎮</text></svg>'), auto"
+  },
+];
